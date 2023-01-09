21d418c4d8af4caa4f2996ace640ed20f3076d29
Raulmoyano2970 <raulmoyano2970@gmail.com>
2023-01-09 17:11:26 -0300

    dev

--- src/url.js
+++ src/url.js
@@ -1,7 +1,8 @@
+// let url = 'http://localhost:8000/api/'
 let url = 'https://backsmoky.onrender.com/api/'
 
-if(process.env.NODE_ENV === "production"){
-    url = process.env.REACT_APP_URL
-}
+// if(process.env.NODE_ENV === "production"){
+//     url = process.env.REACT_APP_URL
+// }
 
 module.exports = url
