36d1c04cc1095a73d6a977e4a3f44d7adc4a2aa6
Raulmoyano2970 <raulmoyano2970@gmail.com>
2024-06-17 20:20:19 -0300

    cargando

--- src/pages/Eliquids.jsx
+++ src/pages/Eliquids.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import CardVaper from '../components/CardVaper';
 import eliquidsActions from '../redux/actions/eliquidsAction';
@@ -8,115 +7,147 @@ import { Fade } from "react-awesome-reveal";
 export default function Eliquids() {
   const dispatch = useDispatch();
   let { eliquids, category } = useSelector((store) => store.eliquidsReducer);
-  let [check, setCheck] = useState([])
-  let [inp, setInp] = useState("")
+  let [check, setCheck] = useState([]);
+  let [inp, setInp] = useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const data = { name: "", category: "" }
-    dispatch(eliquidsActions.getEliquids(data))
-    dispatch(eliquidsActions.getCategories())
-  }, [])
+    const fetchData = async () => {
+      const data = { name: "", category: "" };
+      setLoading(true);
+      await dispatch(eliquidsActions.getEliquids(data));
+      await dispatch(eliquidsActions.getCategories());
+      setLoading(false);
+    };
+    fetchData();
+  }, [dispatch]);
 
-  const search = (e) => {
+  const search = async (e) => {
+    setInp(e.target.value);
+    const data = { name: e.target.value, category: check.toString() };
+    setLoading(true);
+    await dispatch(eliquidsActions.getEliquids(data));
+    setLoading(false);
+  };
 
-    setInp(e.target.value)
-    const data = { name: e.target.value, category: check.toString() }
-    dispatch(eliquidsActions.getEliquids(data))
-  }
-  const categ = (e) => {
+  const categ = async (e) => {
     if (check.includes(e)) {
-      let i = check.indexOf(e)
-      check.splice(i, 1)
-      setCheck(check)
+      let i = check.indexOf(e);
+      check.splice(i, 1);
+      setCheck([...check]);
+    } else {
+      check.push(e);
+      setCheck([...check]);
     }
-    else {
-      check.push(e)
-      setCheck(check)
-    }
-    const y = check.toString()
-    const data = { name: inp, category: y }
-    dispatch(eliquidsActions.getEliquids(data))
-    console.log(check)
-  }
-  console.log(eliquids)
+    const y = check.toString();
+    const data = { name: inp, category: y };
+    setLoading(true);
+    await dispatch(eliquidsActions.getEliquids(data));
+    setLoading(false);
+  };
+
   return (
     <div className="bg-black body-container-products d-flex flex-column flex-wrap gap-3 justify-content-center align-items-center">
       <div className='filter-container d-flex flex-row flex-wrap gap-4 justify-content-center align-items-center'>
         <div className='box'>
-          <div class="search-box">
+          <div className="search-box">
             <input type="text" className='search' placeholder='Search' onChange={search} />
           </div>
         </div>
         <div>
           <label className='label-check gap-4 d-flex'>
-            {category.map(e => <><input class="checkbox" type="checkbox" onClick={e => categ(e.target.value)} value={e} />
-              <div class="checkmark">{e}</div></>)}
+            {category.map(e => (
+              <React.Fragment key={e}>
+                <input className="checkbox" type="checkbox" onClick={() => categ(e)} value={e} />
+                <div className="checkmark">{e}</div>
+              </React.Fragment>
+            ))}
           </label>
         </div>
       </div>
       <Fade>
         <h1 className='title-products'>E-liquids</h1>
         <div className='d-flex flex-wrap gap-5 justify-content-center pt-4 pb-4'>
-          {eliquids.length === 0 ? (<> <img src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" alt="404" width={350} /> </>) : (eliquids.map(e => <CardVaper name={e.name} description={e.description} price={e.price} img={e.photo} cont={e.amount} id={e._id} type={"liquids"} />))}
+          {loading ? (
+            <h4 className='title-products'>Cargando productos...</h4>
+          ) : (
+            eliquids.length === 0 ? (
+              <img src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" alt="404" width={350} />
+            ) : (
+              eliquids.map(e => (
+                <CardVaper
+                  key={e._id}
+                  name={e.name}
+                  description={e.description}
+                  price={e.price}
+                  img={e.photo}
+                  cont={e.amount}
+                  id={e._id}
+                  type={"liquids"}
+                />
+              ))
+            )
+          )}
         </div>
       </Fade>
-      <div class="slider">
-          <div class="slider-track">
-            <div class="slide">
-              <img src="img/carousel/fume.png" alt="logo fume" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/ignite.png" alt="logo ignite" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/joytech.png" alt="joytech logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/nasty.png" alt="nasty logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/smok.png" alt="smok logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/zomo.png" alt="zomo logo" />
-            </div>
-           <div class="slide">
-              <img src="img/carousel/fume.png" alt="logo fume" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/ignite.png" alt="logo ignite" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/joytech.png" alt="joytech logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/nasty.png" alt="nasty logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/smok.png" alt="smok logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/zomo.png" alt="zomo logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
+      <div className="slider">
+        <div className="slider-track">
+          <div className="slide">
+            <img src="img/carousel/fume.png" alt="logo fume" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/ignite.png" alt="logo ignite" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/joytech.png" alt="joytech logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/nasty.png" alt="nasty logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/smok.png" alt="smok logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/zomo.png" alt="zomo logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/fume.png" alt="logo fume" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/ignite.png" alt="logo ignite" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/joytech.png" alt="joytech logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/nasty.png" alt="nasty logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/smok.png" alt="smok logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/zomo.png" alt="zomo logo" />
+          </div>
+          <div className="slide">
+            <img src="img/carousel/geekvape.png" alt="logo geekvape" />
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Vapers.jsx
+++ src/pages/Vapers.jsx
@@ -1,122 +1,153 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import CardVaper from '../components/CardVaper';
-import vapersActions from '../redux/actions/vapersAction'
+import vapersActions from '../redux/actions/vapersAction';
 import { Fade } from "react-awesome-reveal";
-
 
 export default function Vapers() {
     const dispatch = useDispatch();
     let { vapers, category } = useSelector((store) => store.vapersReducer);
-    let [check, setCheck] = useState([])
-    let [inp, setInp] = useState("")
+    let [check, setCheck] = useState([]);
+    let [inp, setInp] = useState("");
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-        const data = { name: "", category: "" }
-        dispatch(vapersActions.getVapers(data))
-        dispatch(vapersActions.getCategories())
-    }, [])
+        const fetchData = async () => {
+            const data = { name: "", category: "" };
+            setLoading(true);
+            await dispatch(vapersActions.getVapers(data));
+            await dispatch(vapersActions.getCategories());
+            setLoading(false);
+        };
+        fetchData();
+    }, [dispatch]);
 
-    const search = (e) => {
+    const search = async (e) => {
+        setInp(e.target.value);
+        const data = { name: e.target.value, category: check.toString() };
+        setLoading(true);
+        await dispatch(vapersActions.getVapers(data));
+        setLoading(false);
+    };
 
-        setInp(e.target.value)
-        const data = { name: e.target.value, category: check.toString() }
-        dispatch(vapersActions.getVapers(data))
-    }
-    const categ = (e) => {
+    const categ = async (e) => {
         if (check.includes(e)) {
-            let i = check.indexOf(e)
-            check.splice(i, 1)
-            setCheck(check)
+            let i = check.indexOf(e);
+            check.splice(i, 1);
+            setCheck(check);
+        } else {
+            check.push(e);
+            setCheck(check);
         }
-        else {
-            check.push(e)
-            setCheck(check)
-        }
-        const y = check.toString()
-        const data = { name: inp, category: y }
-        dispatch(vapersActions.getVapers(data))
-        console.log(check)
-    }
+        const y = check.toString();
+        const data = { name: inp, category: y };
+        setLoading(true);
+        await dispatch(vapersActions.getVapers(data));
+        setLoading(false);
+    };
+
     return (
         <div className="bg-black body-container-products d-flex flex-column flex-wrap gap-3 justify-content-center align-items-center pb-2">
-                <div className='filter-container d-flex flex-row flex-wrap gap-4 justify-content-center align-items-center'>
-                    <div className='box'>
-                        <div class="search-box">
-                            <input type="text" placeholder='Search' className='search' onChange={search} />
-                        </div>
-                    </div>
-                    <div>
-                        <label className='label-check gap-4 d-flex'>
-                            {category.map(e => <><input className="checkbox" type="checkbox" onClick={e => categ(e.target.value)} value={e} />
-                                <div class="checkmark">{e}</div></>)}
-                        </label>
+            <div className='filter-container d-flex flex-row flex-wrap gap-4 justify-content-center align-items-center'>
+                <div className='box'>
+                    <div className="search-box">
+                        <input type="text" placeholder='Search' className='search' onChange={search} />
                     </div>
                 </div>
+                <div>
+                    <label className='label-check gap-4 d-flex'>
+                        {category.map(e => (
+                            <React.Fragment key={e}>
+                                <input className="checkbox" type="checkbox" onClick={() => categ(e)} value={e} />
+                                <div className="checkmark">{e}</div>
+                            </React.Fragment>
+                        ))}
+                    </label>
+                </div>
+            </div>
             <Fade>
                 <h1 className='title-products'>Vapers</h1>
                 <div className='d-flex flex-wrap gap-5 justify-content-center pt-4 pb-4'>
-                    {vapers.length === 0 ? (<> <img src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" alt="404" width={350} /> </>) : (vapers.map(e => <CardVaper name={e.name} description={e.description} price={e.price} img={e.photo} cont={e.amount} id={e._id} type={"vapers"} />))}
+                    {loading ? (
+                        <h4 className='title-products'>Cargando productos...</h4>
+                    ) : (
+                        vapers.length === 0 ? (
+                            <img src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" alt="404" width={350} />
+                        ) : (
+                            vapers.map(e => (
+                                <CardVaper
+                                    key={e._id}
+                                    name={e.name}
+                                    description={e.description}
+                                    price={e.price}
+                                    img={e.photo}
+                                    cont={e.amount}
+                                    id={e._id}
+                                    type={"vapers"}
+                                />
+                            ))
+                        )
+                    )}
                 </div>
             </Fade>
-            <div class="slider">
-          <div class="slider-track">
-            <div class="slide">
-              <img src="img/carousel/fume.png" alt="logo fume" />
+            <div className="slider">
+                <div className="slider-track">
+                    <div className="slide">
+                        <img src="img/carousel/fume.png" alt="logo fume" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/ignite.png" alt="logo ignite" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/joytech.png" alt="joytech logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/nasty.png" alt="nasty logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/smok.png" alt="smok logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/zomo.png" alt="zomo logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/fume.png" alt="logo fume" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/ignite.png" alt="logo ignite" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/joytech.png" alt="joytech logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/nasty.png" alt="nasty logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/smok.png" alt="smok logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/zomo.png" alt="zomo logo" />
+                    </div>
+                    <div className="slide">
+                        <img src="img/carousel/geekvape.png" alt="logo geekvape" />
+                    </div>
+                </div>
             </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/ignite.png" alt="logo ignite" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/joytech.png" alt="joytech logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/nasty.png" alt="nasty logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/smok.png" alt="smok logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/zomo.png" alt="zomo logo" />
-            </div>
-           <div class="slide">
-              <img src="img/carousel/fume.png" alt="logo fume" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/ignite.png" alt="logo ignite" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/joytech.png" alt="joytech logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/nasty.png" alt="nasty logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/smok.png" alt="smok logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/vaporesso.png" alt="vaporesso logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/zomo.png" alt="zomo logo" />
-            </div>
-            <div class="slide">
-              <img src="img/carousel/geekvape.png" alt="logo geekvape" />
-            </div>
-          </div>
         </div>
-        </div>
-    )
+    );
 }
